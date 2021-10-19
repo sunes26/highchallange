c258032407b16d0bdb2d3d96e1ba0385a56ec781
sunes26 <85713817+sunes26@users.noreply.github.com>
2021-10-19 14:51:03 +0900

    Update year.js

--- year.js
+++ year.js
@@ -1,15 +1,20 @@
 const form = document.getElementById('year');
 
-form.addEventListener('submit',function(event){
+form.addEventListener('click',function(event){
     const day = document.getElementById('day').value
     localStorage.setItem('Point',0)
     console.log(day)
-    if(2021-day <=28){
-       location.href = "old/page1.html"
+
+    if(day==0){
+        alert("년도를 입력하세요~")
+        location.href = "main.html"
+    }
+    else if(2021-day <=28){
+        location.href = "old/page1.html"
     }
     else if(2021-day>=29){
        
-          location.href = "youngpage/page1.html"
+        location.href = "youngpage/page1.html"
     }
     event.preventDefault()
 });
