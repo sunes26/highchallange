dbf8c3e98647710b98c1d720abde9d4ae6c47afd
sunes26 <85713817+sunes26@users.noreply.github.com>
2021-10-19 13:08:26 +0900

    Update year.js

--- year.js
+++ year.js
@@ -5,10 +5,11 @@ form.addEventListener('submit',function(event){
     localStorage.setItem('Point',0)
     console.log(day)
     if(2021-day <=28){
-        location.href = "youngpage/page1.html"
+       location.href = "old/page1.html"
     }
     else if(2021-day>=29){
-        location.href = "old/page1.html"
+       
+          location.href = "youngpage/page1.html"
     }
     event.preventDefault()
 });
